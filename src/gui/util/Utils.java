e688c460e3956980680b0e7c5cf4132408a65f0a
Andrepultz <andrepultz2013@gmail.com>
2020-09-13 15:04:51 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -11,5 +11,15 @@ public class Utils {
 		
 	}
 	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	
 
 }
